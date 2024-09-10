45d83f855757b410f3cb7a91c135ee4345960533
Kallebe-Assis <63472419+Kallebe-Assis@users.noreply.github.com>
2024-09-09 21:01:27 -0300

    Miniblog v2
    Cabeçalho e inputs inicialmente estilizados e com alteração de gif

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'; // Importar useState do React
+import React, { useState } from "react"; // Importar useState do React
 import ComponentePosts from "./ComponentePosts";
-
-
+import "./styles.css";
 
 function App() {
-
   const [componenteExibido, setComponenteExibido] = useState(false); // Estado para controlar a exibição
 
   const handleClick = () => {
@@ -14,19 +12,34 @@ function App() {
 
   return (
     <>
-      <h1>MINIBLOG</h1>
-      <img src="https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif" alt="Homer Simpsons"/>
-      <input type="text" placeholder="Insira o título do seu post"></input>
-      <textarea type="text" placeholder="Insira o conteúdo do seu post"></textarea>
-      <input type="url" placeholder="Insira a imagem do seu post (Opcional)"></input>
-      <button onClick={handleClick}>Criar Post</button>
+      <div className="cabecalho">
+        <h1>MINIBLOG</h1>
+        <img
+          src="https://64.media.tumblr.com/9ac5a447780cf383a5797b90bbf9ff9f/e8eab754f1696c68-99/s480x360/ad317a21258991cfb4a4a4341a19fa04e6ec965e.gif"
+          // src="https://pa1.aminoapps.com/6735/eec96583c81bb057c652cc0605d946556a7d841d_hq.gif"
+          alt="Homer Simpsons"
+        />
+      </div>
 
+      <div className="inputs">
+        <input type="text" placeholder="Insira o título do seu post"></input>
+        <textarea
+          type="text"
+          placeholder="Insira o conteúdo do seu post"
+        ></textarea>
+        <input
+          type="url"
+          placeholder="Insira a imagem do seu post (Opcional)"
+        ></input>
+        <button onClick={handleClick} className="botaoCriar">
+          Criar Post
+        </button>
+      </div>
       {componenteExibido && <ComponentePosts />}
 
       {/* <ComponentePosts/> */}
     </>
-  )
-
+  );
 }
 
 export default App;
